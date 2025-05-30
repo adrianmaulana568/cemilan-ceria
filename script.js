4e4f24a117964f0cf165e62dee57ee23ee17bdc7
adrianmmaulana <141643671+adrianmaulana568@users.noreply.github.com>
2025-05-31 06:46:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-// Select DOM Elements
+// Toggle navbar menu
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburger = document.querySelector("#hamburger-menu");
 
-// Toggle "active" class on hamburger menu click
 hamburger.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Close menu when clicking outside
+// Toggle search form
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search");
+
+searchButton.onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+
+// Toggle shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+const cartButton = document.querySelector("#shopping-cart");
+
+cartButton.onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Close dropdowns if clicked outside
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!searchform.contains(e.target) && !searchButton.contains(e.target)) {
+    searchform.classList.remove("active");
+  }
+
+  if (!cartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
